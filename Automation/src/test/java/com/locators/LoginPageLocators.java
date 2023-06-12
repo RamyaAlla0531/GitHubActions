ef871ff1a2633e2f3b4056f52d1474d5a2454df2
RamyaAlla0531 <ramyareddycd315.com>
2023-06-12 18:08:57 +0530

    committing latest code

--- Automation/src/test/java/com/locators/LoginPageLocators.java
+++
@@ -1,9 +0,0 @@
-package com.locators;
-
-import org.openqa.selenium.By;
-
-public interface LoginPageLocators {
-	By Username = By.id("username");
-	By Password = By.id("password");
-	By SignInButton = By.xpath("//button[text()='SIGN IN']");
-}
--- Automation/src/test/java/com/reusablemethods/TimeOuts.java
+++
@@ -1,7 +0,0 @@
-package com.reusablemethods;
-
-public interface TimeOuts {
-	public static final int SHORTWAIT = 15;
-	public static final int MEDIUMWAIT = 30;
-	public static final int LONGWAIT = 60;
-}
